a8822a82f76d020c18e648f921cb8792b7c1f12a
YUZHIGUIYI <13601462025@163.com>
2023-12-14 21:20:09 +0800

    Fix error C2220 in clipper.cpp

--- contrib/clipper/clipper.cpp
+++ contrib/clipper/clipper.cpp
@@ -3910,7 +3910,7 @@ void ClipperOffset::Execute(Paths& solution, double delta)
     clpr.AddPath(outer, ptSubject, true);
     clpr.ReverseSolution(true);
     clpr.Execute(ctUnion, solution, pftNegative, pftNegative);
-    if (solution.size() > 0) solution.erase(solution.begin());
+    if (!solution.empty()) solution.erase(solution.begin());
   }
 }
 //------------------------------------------------------------------------------
